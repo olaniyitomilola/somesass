1ae4ad4630857380cfd2d398feb13c2cf3e07c3a
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2022-09-19 15:50:10 +0100

    front still

--- public/script.js
+++ public/script.js
@@ -1,22 +1,65 @@
-const mediacomt = document.querySelector('#imageviewport');
 
-const navleft = document.getElementById('navleft');
-const navright = document.getElementById('navright');
+
+const imageViewPort = document.querySelector('#imageviewport');
+const adViewPort = document.querySelector('#advertviewport')
+
+//navigators
+const navleft = document.querySelector('.mediabox #navleft');
+const navright = document.querySelector('.mediabox #navright');
+const adnavleft = document.querySelector('.adbox #navleft')
+const adnavright = document.querySelector('.adbox #navright')
+
+
+//width of image box and adpage box
 const images = document.querySelectorAll('#images')
-const boxwidth =  images[0].getBoundingClientRect().width
-const mediaWidth = mediacomt.scrollWidth;
-const extra = mediaWidth - (boxwidth * images.length)
-const offs = extra/images.length
-const theLeft = mediacomt.getClientRects()
+const adpages = document.querySelectorAll('.vados')
 
-navleft.addEventListener('click',()=>{
-    mediacomt.scrollBy(-(boxwidth + offs),0)
+//size of boxes
+const boxwidth =  images[0].getBoundingClientRect().width
+const mediaWidth = imageViewPort.scrollWidth;
+const offs = (mediaWidth - (boxwidth * images.length))/images.length
+
+const adwidth = adpages[0].getBoundingClientRect().width;
+const adviewportwidth = adViewPort.scrollWidth;
+const extra = (adviewportwidth-(adwidth*adpages.length))/adpages.length
+
+
+console.log(adviewportwidth);
+adnavright.addEventListener('click',()=>{
+    adViewPort.scrollBy(adwidth+extra,0)
 })
-navright.addEventListener('click',()=>{
-    mediacomt.scrollBy(boxwidth + offs,0)
+adnavleft.addEventListener('click',()=>{
+    adViewPort.scrollBy(-(adwidth+extra),0)
 })
-if(images[0].getBoundingClientRect().left === 0){
-    navleft.removeEventListener('click',()=>{
-        mediacomt.scrollBy(-(boxwidth + offs),0)
-    });
+
+//navleft.addEventListener('click',scroller(imageViewPort,-(boxwidth+offs)))
+//navright.addEventListener('click',scroller(imageViewPort,boxwidth+offs))
+
+
+
+var content_data = [];
+datagetting(content_data).then(()=>{
+    content_data[0].adcontent.forEach((content)=>{
+        const adipage = document.createElement('div');
+        adipage.className = 'vados';
+        adipage.textcontent = content.header;
+        adipage.style.backgroundColor = content.background
+        adViewPort.appendChild(adipage)
+    })
+});
+
+
+
+
+//functions
+
+const scroller = (view,scrolldist)=>{
+    view.scrollBy(scrolldist,0)
+}
+
+async function datagetting (content_data){
+    const content = await fetch ("../adicontent.json")
+     let contentdata = await content.json()
+    content_data.push(contentdata)
+    return content_data;
 }
